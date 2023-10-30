cc555ae42347c1affd1defb8ed3379810f893579
MichaelCodes123 <126003398+MichaelCodes123@users.noreply.github.com>
2023-10-30 18:36:18 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<main>
+	<div class="container">
+		<div class="upper">
+			<div class="upper__text">
+				<h1 class="heading">
+					Komosu Network		
+				</h1>
+				<p>We Are Experts At Bridging The Gap Between You and Highly Motivated Buyers</p>
+			</div>
+		</div>
+		<div class="lower"></div>
+	</div>
+</main>
+
+<style>
+	.container {
+		height: 100vh;
+		max-width: 100%;
+	}
+	.upper{
+		height: 30vh;
+		max-width: 100%;
+	}
+
+	.lower{
+		height: 70vh;
+		max-width: 100%;
+	}
+
+
+	.upper__text{
+		display: flex;
+		justify-content: space-between;
+		height: 100%;
+		border: 1px #333 solid;
+		padding: 80px;
+		
+		& p{
+			width: 50%;
+		}
+	}
+</style>
